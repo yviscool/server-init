19584305c5398ece6454e98bed4b2feaf67c1683
YungVenuz <5196666qwe@gmail.com>
2022-05-07 18:05:40 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -538,7 +538,7 @@ if [[ ! -d "$HOME/.oh-my-zsh" ]]; then
 
     # install zsh-autosuggestions
     # git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-    git clone https://gitee.com/Coxhuang/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    git clone https://gitee.com/dictxiong/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
     # install zsh-syntax-highlighting
     # git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
